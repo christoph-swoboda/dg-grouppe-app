580c81e2d811d6396e441c0d56f99d402571f889
SeaHawks <tanvirrezaanik@gmail.com>
2022-08-11 04:28:51 +0600

    IOS issues fixed

--- src/components/emptyDashboard.js
+++ src/components/emptyDashboard.js
@@ -1,18 +1,17 @@
 import React from "react";
-import {IonCardTitle} from "@ionic/react";
+import {IonCard, IonCardSubtitle, IonCardTitle, IonToolbar} from "@ionic/react";
 import {checkmarkCircleOutline} from "ionicons/icons";
 
 const EmptyDashboard = ({name}) => {
     return (
-        <div className='dashboardTitle' style={{marginTop:'9rem'}}>
-            <IonCardTitle >Hi {name}</IonCardTitle>
-            <p>All is good, you have 0 requests for today </p>
+        <IonCard className='emptyDashboardTitle'>
             <ion-icon icon={checkmarkCircleOutline}/>
-            <div className='requestStatus'>
-                <h2 >All Is Good </h2>
-                <p >You Dont Have Any Requests </p>
-            </div>
-        </div>
+            <IonCard className='requestStatus'>
+                <IonCardTitle >Hi {name}</IonCardTitle>
+                <IonCardSubtitle >All is good, you have 0 requests for today</IonCardSubtitle>
+                {/* <IonCardSubtitle >You Dont Have Any Requests </IonCardSubtitle> */}
+            </IonCard>
+        </IonCard>
     )
 }
 
